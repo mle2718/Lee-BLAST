0ea59b4e9709188c86edef462c4bd7f16ee9fdce
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2022-11-07 15:12:45 -0500

    Year to year.  fix output path.

--- cod_haddock_fy2023/code/presim/reading_NAA_from_stockassessment.R
+++ cod_haddock_fy2023/code/presim/reading_NAA_from_stockassessment.R
@@ -23,17 +23,17 @@ haddockA_NAA<-as.data.frame(gomhaddock_BASE_2022MT$N.age)
 #Prepend age to the columns
 codA_NAA<-codA_NAA %>% 
   dplyr::rename_with(~ paste0("age", .)) %>%
-  tibble::rownames_to_column(var="Year")
+  tibble::rownames_to_column(var="year")
 
 codB_NAA<-codB_NAA %>% 
   dplyr::rename_with(~ paste0("age", .)) %>%
-  tibble::rownames_to_column(var="Year")
+  tibble::rownames_to_column(var="year")
 
 haddockA_NAA<-haddockA_NAA %>% 
   dplyr::rename_with(~ paste0("age", .)) %>%
-  tibble::rownames_to_column(var="Year")
+  tibble::rownames_to_column(var="year")
 
 
-write_dta(codA_NAA,here("cod_haddock_fy2023","source_data","cod_agepro","codagepro2021", paste0(codA,".dta")))
-write_dta(codB_NAA,here("cod_haddock_fy2023","source_data","cod_agepro","codagepro2021",paste0(codB,".dta")))
-write_dta(haddockA_NAA,here("cod_haddock_fy2023","source_data","haddock_agepro","2022_HAD_GM",paste0(haddockA,"dta")))
+write_dta(codA_NAA,here("cod_haddock_fy2023","source_data","cod agepro","codagepro2021", paste0(codA,".dta")))
+write_dta(codB_NAA,here("cod_haddock_fy2023","source_data","cod agepro","codagepro2021",paste0(codB,".dta")))
+write_dta(haddockA_NAA,here("cod_haddock_fy2023","source_data","haddock agepro","2022_HAD_GM",paste0(haddockA,"dta")))
